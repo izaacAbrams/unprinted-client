412a244bec5b5f261a28c78e7f47f89c66d3dcf0
Izaac Abrams <52928596+izaacAbrams@users.noreply.github.com>
2020-06-24 18:07:04 -0500

    added markdown input to new chapter

--- src/routes/AddChapter/AddChapter.js
+++ src/routes/AddChapter/AddChapter.js
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
 import UnprintedContext from "../../context/UnprintedContext";
+import MyEditor from "../../components/MyEditor/MyEditor";
 
 class AddChapter extends Component {
 	static contextType = UnprintedContext;
 
-	handleSubmit(e) {
-		e.preventDefault();
+	handleSubmit(content) {
 		const newChapter = {
 			section: document.getElementById("AddChapter__chapter").value,
-			content: document.getElementById("AddChapter__content").value,
+			content: content,
 		};
 		this.context.addSection(newChapter, this.props.match.params.book_id);
 		this.props.history.push("/my-books");
@@ -16,13 +16,11 @@ class AddChapter extends Component {
 
 	render() {
 		return (
-			<form onSubmit={(e) => this.handleSubmit(e)} className="AddChapter">
+			<div className="AddChapter">
 				<label htmlFor="AddChapter__chapter">Chapter:</label>
 				<input type="text" id="AddChapter__chapter" />
-				<label htmlFor="AddChapter__content">Content:</label>
-				<textarea id="AddChapter__content"></textarea>
-				<button type="submit">Submit</button>
-			</form>
+				<MyEditor handleSubmit={(content) => this.handleSubmit(content)} />
+			</div>
 		);
 	}
 }
