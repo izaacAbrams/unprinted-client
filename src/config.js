8fd6b45061153857970ad7e98e399d6a94baca38
Izaac Abrams <52928596+izaacAbrams@users.noreply.github.com>
2020-07-04 18:20:11 -0500

    fixed api endpoint config

--- src/config.js
+++ src/config.js
@@ -2,8 +2,7 @@ export default {
 	TOKEN_KEY: "unprinted-client",
 	STRIPE_ENDPOINT:
 		"https://dashboard.stripe.com/express/oauth/authorize?response_type=code&client_id=ca_HX1T5mnB86us4ScnvcGNTrsZOcfT6Amv&scope=read_write",
-	// API_ENDPOINT: "https://evening-tundra-86929.herokuapp.com/api",
-	API_ENDPOINT: "http://localhost:8000/api",
+	API_ENDPOINT: "https://evening-tundra-86929.herokuapp.com/api",
 	STRIPE_PROMISE_LIVE:
 		"pk_live_51Gxx6HLzmqWmrXIYrarPiITkJoxDMAYRg3xxNfSnCFxC3bkoBfnZWgDjToMzjS5AuzJ6TZToYQcc66ZjWVVa8zSF00ukINoJVb",
 	STRIPE_PROMISE:
